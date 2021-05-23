47c08f5003377f2707ac9e7842809ea3f05c5968
Dima Kolesnik <admin@itisvpn.com>
2021-05-23 18:28:52 +0300

    lesson2

---
+++ js/alert.js
@@ -0,0 +1,9 @@
+const first_number = prompt("Введите первое число");
+const second_number = prompt("Введите второе число");
+const answer = first_number - second_number;
+alert(answer);
+
+const first_name = prompt("Введите имя");
+const last_name = prompt("Введите фамилию");
+const patronymic = prompt("Введите отчество");
+alert(first_name + " " + last_name + " " + patronymic)
